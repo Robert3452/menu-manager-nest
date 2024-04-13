a75d9440567c173125d654d47fbe373a36d29cb1
Robert3452 <romeza1239@gmail.com>
2024-04-13 12:44:54 -0500

    First steps, serverless configuration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,11 +3,14 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
-    console.log('fffff');
     return this.appService.getHello();
+  }
+  @Get('test')
+  getHelloD() {
+    return { message: 'GGGG' };
   }
 }
